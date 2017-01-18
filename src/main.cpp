0282dc0ea7137b948f4ef17affe999e22e7f78e1
izhengfan <mrzhengfan@gmail.com>
2017-01-18 17:19:41 +0800

    we don't need ros

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,17 @@
 #include "type.h"
 #include "config.h"
 
-#include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
-#include <std_msgs/Header.h>
-#include <std_msgs/Float32MultiArray.h>
-#include <geometry_msgs/Pose.h>
-#include <sensor_msgs/Image.h>
-#include <visualization_msgs/Marker.h>
-//#include <tf/transform_broadcaster.h>
-//#include <tf/transform_datatypes.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
+//#include <ros/ros.h>
+//#include <nav_msgs/Odometry.h>
+//#include <std_msgs/Header.h>
+//#include <std_msgs/Float32MultiArray.h>
+//#include <geometry_msgs/Pose.h>
+//#include <sensor_msgs/Image.h>
+//#include <visualization_msgs/Marker.h>
+////#include <tf/transform_broadcaster.h>
+////#include <tf/transform_datatypes.h>
+//#include <image_transport/image_transport.h>
+//#include <cv_bridge/cv_bridge.h>
 
 using namespace std;
 using namespace cv;
@@ -35,13 +35,13 @@ int main(int argc, char **argv) {
     int numFrame = atoi(argv[2]);
     double markerSize = atof(argv[3]);
 
-    //! Init ros
-    ros::init(argc, argv, "pub");
-    ros::start();
-    ros::NodeHandle nh;
-    ros::Rate rate(100);
-    image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise("/camera/image_raw",1);
+//    //! Init ros
+//    ros::init(argc, argv, "pub");
+//    ros::start();
+//    ros::NodeHandle nh;
+//    ros::Rate rate(100);
+//    image_transport::ImageTransport it(nh);
+//    image_transport::Publisher pub = it.advertise("/camera/image_raw",1);
 
     //! Init config
     Config::InitConfig(strFolderPathMain, numFrame, markerSize);
